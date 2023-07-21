c9b7cdef333ea1d5288a31671ba1463b62b6c838
kneil <kneil@users.noreply.github.com>
2023-07-21 16:16:46 +0300

    Update main.go

--- 2/main.go
+++ 2/main.go
@@ -33,89 +33,9 @@ func main() {
 	// We don't want to see the plugin logs.
 	log.SetOutput(ioutil.Discard)
 
-	// 	// We're a host. Start by launching the plugin process.
-	// 	client := plugin.NewClient(&plugin.ClientConfig{
-	// 		HandshakeConfig: shared.Handshake,
-	// 		Plugins:         shared.PluginMap,
-	// 		Cmd:             exec.Command("sh", "-c", os.Getenv("COUNTER_PLUGIN")),
-	// 		AllowedProtocols: []plugin.Protocol{
-	// 			plugin.ProtocolNetRPC, plugin.ProtocolGRPC},
-	// 	})
-
-	// 	clientMulter := plugin.NewClient(&plugin.ClientConfig{
-	// 		HandshakeConfig: multer.Handshake,
-	// 		Plugins:         multer.PluginMap,
-	// 		Cmd:             exec.Command("sh", "-c", os.Getenv("MULTER_PLUGIN")),
-	// 		AllowedProtocols: []plugin.Protocol{
-	// 			plugin.ProtocolNetRPC, plugin.ProtocolGRPC},
-	// 	})
-
-	//    /* Client required to connect to plugin */
-	// 	clientFrontend := plugin.NewClient(&plugin.ClientConfig{
-	// 		HandshakeConfig: frontend.Handshake,
-	// 		Plugins:         frontend.PluginMap,
-	// 		Cmd:             exec.Command("sh", "-c", os.Getenv("FRONTEND_PLUGIN")),
-	// 		AllowedProtocols: []plugin.Protocol{
-	// 			plugin.ProtocolNetRPC, plugin.ProtocolGRPC},
-	// 	})
-	// 	defer client.Kill()
-	// 	defer clientMulter.Kill()
-	// 	defer clientFrontend.Kill()
-
-	// // Connect via RPC
-	// rpcClient, err := client.Client()
-	// if err != nil {
-	// 	fmt.Println("Error:", err.Error())
-	// 	os.Exit(1)
-	// }
-
-	// // Request the plugin
-	// raw, err := rpcClient.Dispense("counter")
-	// if err != nil {
-	// 	fmt.Println("Error:", err.Error())
-	// 	os.Exit(1)
-	// }
-
-	// For multer:
-
-	// Connect via RPC
-	// rpcClientMulter, err := clientMulter.Client()
-	// if err != nil {
-	// 	fmt.Println("Error:", err.Error())
-	// 	os.Exit(1)
-	// }
-
-	// // Request the plugin
-	// rawMult, err := rpcClientMulter.Dispense("multer")
-	// if err != nil {
-	// 	fmt.Println("Error:", err.Error())
-	// 	os.Exit(1)
-	// }
-
-	// For frontend
-
-	// rpcClientFrontend, err := clientFrontend.Client()
-	// if err != nil {
-	// 	fmt.Println("Error:", err.Error())
-	// 	os.Exit(1)
-	// }
-
-	// Request the plugin
-	// rawFrontend, err := rpcClientFrontend.Dispense("frontend")
-	// if err != nil {
-	// 	fmt.Println("Error:", err.Error())
-	// 	os.Exit(1)
-	// }
-
-	// We should have a Counter store now! This feels like a normal interface
-	// implementation but is in fact over an RPC connection.
-	//counter := raw.(shared.Counter)
-
 	counter := LoadPlugin(shared.Handshake, shared.PluginMap, "COUNTER_PLUGIN").(shared.Counter)
-	//multer := rawMult.(multer.Multer)
 
 	multer := LoadPlugin(multer.Handshake, multer.PluginMap, "MULTER_PLUGIN").(multer.Multer)
-	//frontend := rawFrontend.(frontend.Frontend)
 
 	frontend := LoadPlugin(frontend.Handshake, frontend.PluginMap, "FRONTEND_PLUGIN").(frontend.Frontend)
 
@@ -189,18 +109,18 @@ func main() {
 	}
 }
 
-func LoadPlugins() {
+func LoadPlugins() /*map[string]interrface{} */ {
 
 	plugin := LoadPlugin(shared.Handshake, shared.PluginMap, "COUNTER_PLUGIN").(shared.Counter)
 	frontend := LoadPlugin(frontend.Handshake, frontend.PluginMap, "FRONTEND_PLUGIN").(frontend.Frontend)
 	plugin.Get("Hello")
 	frontend.Build("123")
-
+	// return a map of plugins ???
 }
+
 func LoadPlugin(
 	handshake plugin.HandshakeConfig,
 	pluginMap map[string]plugin.Plugin,
-	// pluginStruct interface{},
 	PathVariable string) interface{} {
 
 	client := plugin.NewClient(&plugin.ClientConfig{
@@ -227,8 +147,6 @@ func LoadPlugin(
 		os.Exit(1)
 	}
 
-	// pluginType := reflect.TypeOf (pluginStruct )
-	// returnType := raw.(pluginStruct)
 	return raw
 
 }
